79c1b0e9c4e062475d574adc273303d2b5689d8f
Garrett Singer <gesinger@gmail.com>
2014-03-10 06:23:27 -0400

    Added SR file names to index

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ if (!commander.dir) {
 }
 
 console.log(commander.dir);
+
+var FOOD_DESCRIPTION_FILE            = commander.dir + '/FOOD_DES.txt';
+var NUTRIENT_DATA_FILE               = commander.dir + '/NUT_DATA.txt';
+var WEIGHT_FILE                      = commander.dir + '/WEIGHT.txt';
+var FOOTNOTE_FILE                    = commander.dir + '/FOOTNOTE.txt';
+var FOOD_DESCRIPTION_FILE            = commander.dir + '/FOOD_DES.txt';
+var FOOD_GROUP_DESCRIPTION_FILE      = commander.dir + '/FD_GROUP.txt';
+var LANGUAL_FACTOR_FILE              = commander.dir + '/LANGUAL.txt';
+var LANGUAL_FACTORS_DESCRIPTION_FILE = commander.dir + '/LANGDESC.txt';
+var NUTRIENT_DEFINITION_FILE         = commander.dir + '/NUTR_DEF.txt';
+var SOURCE_CODE_FILE                 = commander.dir + '/SRC_CD.txt';
+var DATA_DERIVATION_DESCRIPTION_FILE = commander.dir + '/DERIV_CD.txt';
+var SOURCES_OF_DATA_FILE             = commander.dir + '/DATA_SRC.txt';
+var SOURCES_OF_DATA_LINK_FILE        = commander.dir + '/DATSRCLN.txt';
