69e08ffd7d4cf49726ffeaa227c08a78e8882108
Garrett Singer <gesinger@gmail.com>
2014-03-10 06:09:27 -0400

    Display help when dir isn't passed

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ var commander = require('commander');
 
 commander
   .version('0.0.1')
-  .option('-d, --dir', 'Path to SR directory')
+  .option('-d, --dir <dir>', 'Path to SR directory')
   .parse(process.argv);
+
+if (!commander.dir) {
+  commander.help();
+}
+
+console.log(commander.dir);
