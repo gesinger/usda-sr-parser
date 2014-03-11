f75090f4d2b93a43f178410d29a60734c0b06876
Garrett Singer <gesinger@gmail.com>
2014-03-10 21:31:36 -0400

    Added file splitter

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
 var commander = require('commander');
+var util = require('util');
+var srFileSplitter = require('./sr-file-splitter');
 
 commander
   .version('0.0.1')
   .option('-d, --sr-dir <dir>', 'Path to SR directory')
-  .option('-m, --mappings-file <file>', 'Path to mappings file')
+  .option('-m, --mappings-file [file]', 'Path to mappings file')
   .parse(process.argv);
 
-if (!commander.dir) {
+var srDir = commander.srDir;
+if (!srDir) {
   commander.help();
 }
 
-console.log(commander.dir);
+var FOOD_DESCRIPTION_FILE            = srDir + '/FOOD_DES.txt';
+var NUTRIENT_DATA_FILE               = srDir + '/NUT_DATA.txt';
+var WEIGHT_FILE                      = srDir + '/WEIGHT.txt';
+var FOOTNOTE_FILE                    = srDir + '/FOOTNOTE.txt';
+var FOOD_DESCRIPTION_FILE            = srDir + '/FOOD_DES.txt';
+var FOOD_GROUP_DESCRIPTION_FILE      = srDir + '/FD_GROUP.txt';
+var LANGUAL_FACTOR_FILE              = srDir + '/LANGUAL.txt';
+var LANGUAL_FACTORS_DESCRIPTION_FILE = srDir + '/LANGDESC.txt';
+var NUTRIENT_DEFINITION_FILE         = srDir + '/NUTR_DEF.txt';
+var SOURCE_CODE_FILE                 = srDir + '/SRC_CD.txt';
+var DATA_DERIVATION_DESCRIPTION_FILE = srDir + '/DERIV_CD.txt';
+var SOURCES_OF_DATA_FILE             = srDir + '/DATA_SRC.txt';
+var SOURCES_OF_DATA_LINK_FILE        = srDir + '/DATSRCLN.txt';
 
-var FOOD_DESCRIPTION_FILE            = commander.dir + '/FOOD_DES.txt';
-var NUTRIENT_DATA_FILE               = commander.dir + '/NUT_DATA.txt';
-var WEIGHT_FILE                      = commander.dir + '/WEIGHT.txt';
-var FOOTNOTE_FILE                    = commander.dir + '/FOOTNOTE.txt';
-var FOOD_DESCRIPTION_FILE            = commander.dir + '/FOOD_DES.txt';
-var FOOD_GROUP_DESCRIPTION_FILE      = commander.dir + '/FD_GROUP.txt';
-var LANGUAL_FACTOR_FILE              = commander.dir + '/LANGUAL.txt';
-var LANGUAL_FACTORS_DESCRIPTION_FILE = commander.dir + '/LANGDESC.txt';
-var NUTRIENT_DEFINITION_FILE         = commander.dir + '/NUTR_DEF.txt';
-var SOURCE_CODE_FILE                 = commander.dir + '/SRC_CD.txt';
-var DATA_DERIVATION_DESCRIPTION_FILE = commander.dir + '/DERIV_CD.txt';
-var SOURCES_OF_DATA_FILE             = commander.dir + '/DATA_SRC.txt';
-var SOURCES_OF_DATA_LINK_FILE        = commander.dir + '/DATSRCLN.txt';
+var foodDescriptions = srFileSplitter(
+  FOOD_DESCRIPTION_FILE,
+  ['ndbNumber', 'foodGroupCode', 'longDescription'],
+  0, function(err, data) {
+  if (err) throw err;
+
+  console.log(util.inspect(data));
+});
+
---
+++ sr-file-splitter.js
@@ -0,0 +1,40 @@
+var fs = require('fs');
+
+module.exports = function(filePath, colNames, colKeyNum, cb) {
+  function cleanCol(dirtyCol) {
+    return dirtyCol 
+             .replace('\r', '')
+             .replace(/^~/, '')
+             .replace(/~$/, '');
+  }
+
+  function objectFromCols(cols, colNames) {
+    var object = {};
+    var i;
+    for (i=0; i<colNames.length; ++i) {
+      object[colNames[i]] = cols[i];
+    }
+    return object;
+  }
+
+  fs.readFile(filePath, function(err, data) {
+    if (err) return cb(err) ;
+
+    var result = {};
+
+    var lines = data.toString().split("\n");
+    lines.forEach(function(line) {
+      var cols = line.toString().split("^");
+      var cleanCols = [];
+      cols.forEach(function(col) {
+        cleanCols.push(cleanCol(col));
+      });
+
+      if (cleanCols[colKeyNum]) {
+        result[cleanCols[colKeyNum]] = objectFromCols(cleanCols, colNames);
+      }
+    });
+
+    cb(null, result);
+  });
+};
