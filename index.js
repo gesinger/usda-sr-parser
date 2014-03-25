84e9d28479a5bf32e9fb958fc76d14419cd89e3e
Garrett Singer <gesinger@gmail.com>
2014-03-25 06:16:17 -0400

    Added option to reuse DB

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ commander
   .version('0.0.1')
   .option('-d, --sr-dir <dir>', 'Path to SR directory')
   .option('-s, --sqlite-db [file]', 'Path to create or re-use sqlite DB')
+  .option('-r, --reuse-db', 'Re-use (don\'t create) the provided sqlite DB')
   .option('-e, --es-export [file]', 'Path to create elasticsearch export')
   .option('-m, --mappings-file [file]', 'Path to mappings file')
   .parse(process.argv);
@@ -17,5 +18,6 @@ if (!srDir) {
 var creator = new SrCreator(srDir);
 creator.generate(
   commander.sqliteDb,
+  commander.reuseDb,
   commander.esExport,
   commander.mappingsFile);
