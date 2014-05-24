fb069a4c3a4e5819dfc6eb132f5a54ef6f8743d6
Garrett Singer <gesinger@gmail.com>
2014-05-24 09:10:32 -0400

    Provide default value for mappings file

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var commander = require('commander');
 var creator = require('./lib/creator');
+var path = require('path');
 
 commander
   .version('0.0.1')
   .option('-d, --sr-dir <dir>', 'Path to SR directory')
   .option('-s, --sqlite-db [file]', 'Path to create or re-use sqlite DB')
   .option('-r, --reuse-db', 'Re-use (don\'t create) the provided sqlite DB')
-  .option('-f, --fill-tables [tables]', 'Only fill listed tables in the sqlite DB')
+  .option(
+    '-f, --fill-tables [tables]', 'Only fill listed tables in the sqlite DB')
   .option('-e, --es-export [file]', 'Path to create elasticsearch export')
   .option('-m, --mappings-file [file]', 'Path to mappings file')
   .parse(process.argv);
@@ -21,11 +23,16 @@ if (fillTables) {
   fillTables = JSON.parse(fillTables);
 }
 
+var mappingsFilePath = commander.mappingsFile;
+if (!commander.mappingsFile) {
+  mappingsFilePath = path.resolve('config/mappings.json');
+}
+
 var creator = new creator(srDir);
 creator.generate({
   dbPath: commander.sqliteDb,
   reuseDb: commander.reuseDb,
   fillTables: fillTables,
   esExportPath: commander.esExport,
-  mappingsFilePath: commander.mappingsFile
+  mappingsFilePath: mappingsFilePath
 });
